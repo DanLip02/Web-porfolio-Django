e65bdb1ced49ceb31c6226d07645a307de161a58
DanLip02 <danchik-lip@mail.ru>
2025-12-12 14:30:54 +0300

    added new custom view to contact apge

--- portfolio_project/portfolio/urls.py
+++ portfolio_project/portfolio/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-urlpatterns = [path('', views.home, name='home'),
+urlpatterns = [path('home/', views.home, name='home'),
                path('projects/', views.project_tree, name='projects_tree'),
                path('about/', views.about, name='about'),
-               path('contacts/', views.contacts, name='contacts')] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+               path('contact/', views.contacts, name='contact')] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
